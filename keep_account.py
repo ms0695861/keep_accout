64e7694579a997fba5bdee2abfcc9bc56d0fe4e6
Martin Wang <ms0695961@gmail.com>
2021-06-09 11:05:16 +0800

    add function

--- keep_account.py
+++ keep_account.py
@@ -1,35 +1,48 @@
 import os 
-#creat empty list
-products = []
 
-
-if os.path.isfile('products.csv'): #check the file is exist or not
-	#read csv
-	with open('products.csv', 'r', encoding = 'utf-8') as f: #utf-8 is common encode type.
+#read file
+def read_file(filename):
+	products = [] #creat empty list
+	with open(filename, 'r', encoding = 'utf-8') as f: #utf-8 is common encode type.
 		for line in f:
 			if 'products,price' in line:
 				continue
 			name, price = line.strip().split(',') #remove '\n' and split ','
 			products.append([name, price])
-else:
-	print("Can't not find file")
-
-
+	return products
 
 #enter your products and price
-while True:
-	name = input('Please enter product name(If want to quit please enter "q"):　')
-	if name == 'q':
-		break
-	price = input('Please enter the price: ')
-	products.append([name, price])
+def usr_input(products):
+	while True:
+		name = input('Please enter product name(If want to quit please enter "q"):　')
+		if name == 'q':
+			break
+		price = input('Please enter the price: ')
+		products.append([name, price])
+	return products
 
 #print purchase record
-for p in products:
-	print(p[0], 'price is ', p[1])
+def print_purchase(products):
+	for p in products:
+		print(p[0], 'price is ', p[1])
 
 #add new data back to csv
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('products' + ',' + 'price' + '\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('products' + ',' + 'price' + '\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+
+def main(filename):
+	if os.path.isfile(filename): #check the file is exist or not
+		products = read_file(filename)					
+	else:
+		print("Can't not find file")
+	products = usr_input(products)
+	print_purchase(products)
+	write_file(filename, products)
+
+filename = 'products.csv'	
+main('products.csv')
+
